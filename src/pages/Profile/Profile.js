74df550e2b7c8b2c556bb6dfb360d92212fadcd2
mosaek2 <mosaek2@gmail.com>
2024-11-06 19:49:35 +0900

    lepisode

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -6,7 +6,7 @@ import Title from '../../components/Title/Title';
 import './Profile.css';
 
 export default function Profile() {
-    const [targetCompany] = useState('web dev company');
+    const [targetCompany] = useState('lepisode');
     const expandTimer = 1000;
     const typeTimer = 2000;
     const typeSpeed = 100;
